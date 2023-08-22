415c8b254e53272df9273ce3738d47fbca78a593
Letícia Carneiro <111892940+Lehcarneiro@users.noreply.github.com>
2023-08-22 14:17:34 -0300

    Update ex8.java

--- Java/Lista-Exercicios/ex8.java
+++ Java/Lista-Exercicios/ex8.java
@@ -7,7 +7,24 @@ public class ex8 {
 		//Criar o algoritmo que possa entrar com nome do produto e valor da compra e imprimir o
 		//nome do produto e o valor da venda.
 		
-		
+	Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do produto: ");
+        String nomeProduto = scanner.nextLine();
+
+        System.out.print("Digite o valor da compra: ");
+        double valorCompra = scanner.nextDouble();
+
+        // Definindo uma margem de lucro de 20%
+        double margemLucro = 0.2;
+
+        // Calculando o valor da venda
+        double valorVenda = valorCompra * (1 + margemLucro);
+
+        System.out.println("\nNome do produto: " + nomeProduto);
+        System.out.println("Valor da venda: " + valorVenda);
+
+        scanner.close();
 		
 		
 		
