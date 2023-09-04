51ead72bb9d9a36f099dcc33b28f6c629edd961f
Letícia Carneiro <111892940+Lehcarneiro@users.noreply.github.com>
2023-09-04 08:05:55 -0300

    Update ex19.c

--- C/ex19.c
+++ C/ex19.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-
-int main(void) {
 
  // Em um campeonato nacional de arco-e-flecha, tem-se equipes de três jogadores
 //para cada estado. Sabendo-se que os arqueiros de uma equipe não obtiveram o
@@ -11,38 +8,40 @@ int main(void) {
 //• Se a soma dos pontos for maior do que 100, imprimir a média aritmética
 //entre eles, caso contrário, imprimir a mensagem "Equipe desclassificada".
 
-  int aux, p1, p2, p3;
-  printf("Jogador 1:");
-  scanf("%d", &p1);
-  printf("Jogador 2:");
-  scanf("%d", &p2);
-  printf("Jogador 3:");
-  scanf("%d", &p3);
+ #include <stdio.h>
 
-  if (p1 < p3 || p1 < p3) {
-    if (p2 < p3) {
-      aux = p3;
-      p3 = p1;
-      p1 = p3;
+ main() {
+  float j1,j2,j3, aux, soma;
+  printf("Pontuação do Jogador 1: ");
+  scanf("%f", &j1);
+  printf("Pontuação do Jogador 2: ");
+  scanf("%f", &j2);
+  printf("Pontuação do Jogador 3: ");
+  scanf("%f", &j3);
+ 
+  if (j1<j2 || j1<j3){ //Se V, j1 não é o maior
+    if (j3<j2){//Se V, j2 é o maior
+          aux = j1;
+          j1 = j2;
+          j2 = aux;
     }
-    if (p3 < p2) {
-      aux = p2;
-      p2 = p1;
-      p1 = aux;
+    else{//j3 é o maior
+          aux = j1;
+          j1 = j3;
+          j3 = aux;
     }
-    if (p2 > p1) {
-      aux = p2;
-      p2 = p1;
-      p1 = aux;
-    }
-    printf("valores:%d,%d,%d," p1, p2, p3);
   }
-  aux = p1+p2+p3;
-  if(aux>=100){
-    printf("Media:%d equipe classificada"aux<3);
-  }else{
-    printf("Equipe desclassifcada")
-    }
-
-  return 0;
+  if(j2 < j3){// Se j2 for menor que j3, troca os valores
+     aux = j2;
+     j2 = j3;
+     j3 = aux;
+  }
+printf("\n - %f - %f - %f -",j1,j2,j3);
+  soma = j1+j2+j3;
+  if(soma>=100){
+    printf("\nEquipe Classificada \nMedia = %f", soma /3);
+  }
+  else {
+    printf("Equipe Desclassificada");
+  }
 }
